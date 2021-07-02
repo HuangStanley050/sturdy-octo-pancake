518c35dcde7641dca64ce72dda6a1ce0645ec97c
Stanley Huang <infamous_godhand@yahoo.com>
2021-07-02 10:44:46 +1000

    custom http hook done

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,1 +1,33 @@
-export const useHttp = () => {};
+import { useState } from "react";
+
+export const useHttp = (requestConfig, applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        request: requestConfig.method,
+        headers: requestConfig.headers,
+        body: JSON.stringify(requestConfig.body),
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
